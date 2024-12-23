2caa8fefc3e1015f54616b6fe1fc7a00ef3cf894
youness <younesserbai@gmail.com>
2024-12-23 09:25:44 +0100

    remove the base name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/youness-portfolio/',
+  base: '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
